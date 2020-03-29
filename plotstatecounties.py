1ce39fb2ba3a6c0bed6e502def36871eda4fb273
Evan Severson <severson.evan@gmail.com>
2020-03-28 20:27:50 -0700

    Show cases per 1 million population

--- plotstatecounties.py
+++ plotstatecounties.py
@@ -1,7 +1,17 @@
+import csv
 import datetime
 import urllib.request
 
 import matplotlib.pyplot as plt
+
+# https://www2.census.gov/programs-surveys/popest/datasets/2010-2019/state/detail/SCPRC-EST2019-18+POP-RES.csv
+populationMap = {}
+with open("co-est2019-alldata.csv") as csvfile:
+    readCSV = csv.reader(csvfile, delimiter=",")
+    for row in readCSV:
+        if row[6].endswith(" County"):
+            row[6] = row[6][:-7]
+        populationMap[(row[5], row[6])] = row[18]
 
 stateMap = {}
 iterator = iter(urllib.request.urlopen("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv"))
@@ -23,11 +33,11 @@ for line in iterator:
         countyMap[county] = []
     countyMap[county].append((d[0], d[4]))
 
-state = "Minnesota"
-counties = ["Carver", "Hennepin", "Ramsey", "Dakota", "Washington"]
+# state = "Minnesota"
+# counties = ["Carver", "Hennepin", "Ramsey", "Dakota", "Washington"]
 
-# state = "California"
-# counties = ["Santa Clara", "San Mateo", "Los Angeles", "San Francisco", "San Diego"]
+state = "California"
+counties = ["Santa Clara", "San Mateo", "Los Angeles", "San Francisco", "San Diego"]
 
 # state = "Washington"
 # counties = ["King", "Snohomish", "Pierce"]
@@ -35,7 +45,7 @@ counties = ["Carver", "Hennepin", "Ramsey", "Dakota", "Washington"]
 # plot state
 stateCaseCounts = stateMap[state]
 x = [datetime.datetime.strptime(i[0], "%Y-%m-%d").date() for i in stateCaseCounts]
-y = [int(i[1]) for i in stateCaseCounts]
+y = [int(i[1]) * (10 ** 6) / int(populationMap[(state, state)]) for i in stateCaseCounts]
 
 plt.locator_params(axis="y", nbins=25)
 
@@ -46,7 +56,7 @@ i = 0
 for county in counties:
     countyCaseCounts = countyMap[(state, county)]
     x = [datetime.datetime.strptime(i[0], "%Y-%m-%d").date() for i in countyCaseCounts]
-    y = [int(i[1]) for i in countyCaseCounts]
+    y = [int(i[1]) * (10 ** 6) / int(populationMap[(state, county)]) for i in countyCaseCounts]
     plt.plot(x, y, marker="o", color="C%s" % i, label=county)
     i = i + 1
 
@@ -54,6 +64,6 @@ for county in counties:
 plt.legend(loc="upper left")
 plt.grid(axis="both")
 plt.ylim(bottom=0)
-plt.title(state + " Covid 19 case trend. Data from NYT https://github.com/nytimes/covid-19-data. By Evan Severson")
-
+plt.title(state + "Covid 19 cases per 1 million population. Data from NYT https://github.com/nytimes/covid-19-data "
+                  "and US census. By Evan Severson")
 plt.show()
